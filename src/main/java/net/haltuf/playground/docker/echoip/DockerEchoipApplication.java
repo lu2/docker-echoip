a53f3f89180ec04b5c22258260d56404a6bf478e
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2017-04-09 18:48:32 +0200

    Spring boot starter generated skeleton

---
+++ src/main/java/net/haltuf/playground/docker/echoip/DockerEchoipApplication.java
@@ -0,0 +1,12 @@
+package net.haltuf.playground.docker.echoip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEchoipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEchoipApplication.class, args);
+	}
+}
